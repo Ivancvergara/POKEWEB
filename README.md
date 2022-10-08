658d2782807d1d9d0d4a3ab2f4cab50b32779bd7
Iván Cuevas <irving_ivan1906@hotmail.com>
2022-10-08 15:47:30 -0500

    add file

---
+++ README.md
@@ -0,0 +1,10 @@
+# POKEWEB
+
+## Información General
+Página Web sobre Pokémon para el Proyecto Final del Bootcamp "Conviértete en Frontend Developer" de Código Facilito.
+
+## Tecnologías / Lenguajes 
+Una lista de tecnologías usadas en este proyecto:
+* HTML
+* CSS
+* JavaScript
---
+++ js/nav.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY > 0);
+})
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+document.getElementById("back-menu").addEventListener("click", ocultar_menu)
+
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back-menu")
+
+function mostrar_menu(){
+    nav.style.left = "0px";
+    background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+    nav.style.right = "-250px"
+    background_menu.style.display = "none"
+}
