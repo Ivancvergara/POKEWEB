50b63ceaeb64820a080a396082afb5d3be857304
Iván Cuevas <irving_ivan1906@hotmail.com>
2022-10-23 14:48:07 -0500

    modified:   css/index.css 	modified:   css/nav.css 	modified:   css/pokedex.css 	modified:   css/pokemones.css 	modified:   css/styles.css 	modified:   html/entrenadores.html 	modified:   html/historia.html 	modified:   html/pokedex.html 	modified:   html/pokemones.html 	modified:   index.html 	modified:   js/nav.js

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,8 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("abajo", window.scrollY > 0);
 })
 
+
+
 const nav = document.getElementById('nav')
 const bg_menu = document.getElementById("back-menu")
 
@@ -19,23 +21,3 @@ bg_menu.addEventListener('click',e => {
     bg_menu.classList.remove('back-menu');
   }
 })
-
-// document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
-
-// nav = document.getElementById("nav");
-// background_menu = document.getElementById("back-menu")
-// btn_menu = document.getElementById("btn_menu")
-
-// function mostrar_menu(){
-//     nav.style.left = "0px";
-//     background_menu.style.display = "block";
-//     btn_menu.id = "cerrar_menu"
-// }
-
-// document.getElementsByClassName("cerar_menu").addEventListener("click", ocultar_menu);
-
-// function ocultar_menu(){
-//     nav.style.left = "-200px"
-//     background_menu.style.display = "none"
-//     document.getElementById("cerrar_menu").id = "btn_menu"
-// }
